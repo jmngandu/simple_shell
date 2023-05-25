629953e8fdb8136f79e4716a7e3f5aa7332d73ca
James Ngandu <jamesmngandu@gmail.com>
2023-05-24 17:42:04 -0700

    added the boilerplate code

---
+++ shell.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+ * main - my shell program
+ *@ac: is the arguement count
+ *@argv: an array of null terminated strings
+ * return: 0 on success
+*/
+int main(int ac, char **argv)
+{
+        (void)ac;(void)argv;
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+void prompt(void);
+#endif
