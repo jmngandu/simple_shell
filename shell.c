b204a8624ad949388b471ae919d3c782095761b2
Ronald Mutua <ronaldmutua617@gmail.com>
2023-05-26 11:51:56 -0700

    modified the shell.c file

--- shell.c
+++ shell.c
@@ -5,9 +5,59 @@
  *@argv: an array of null terminated strings
  * Return: 0 on success
 */
-int main(int ac, char **av, char **env)
+int main(int ac, char **argv)
 {
-	if(ac == 1)
-		prompt(av, env);
+	char *token, *prompt = "cisfun$ ";
+	char *userInput = NULL, *userInput_copy = NULL;
+	size_t n = 0;
+	const char *delim = " \n";
+	int numtokens = 0;
+	int i;
+	pid_t pid;
+	(void)ac;
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 8);
+		if (getline(&userInput, &n, stdin) == -1)
+		{
+			free(userInput);
+			exit(EXIT_FAILURE);
+		}
+		userInput_copy = malloc(sizeof(char) * n);
+		if (userInput_copy == NULL)
+		{
+			perror("memory allocation error");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(userInput_copy, userInput);
+		token = strtok(userInput, delim);
+		while (token)
+		{
+			numtokens++;
+			token = strtok(NULL, delim);
+		}
+		numtokens++;
+		argv = malloc(sizeof(char *) * numtokens);
+		token = strtok(userInput_copy, delim);
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+		pid = fork();
+		if (pid == -1)
+		{
+			free(userInput);
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0)
+			exec(argv);
+		else
+			wait(NULL);
+	}
+	free(userInput_copy);
+	free(userInput);
 	return (0);
 }
